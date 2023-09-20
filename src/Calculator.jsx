752f4b0c903227530e9e7d2e87e6bf36e199d590
მაცი <93665709+maci03@users.noreply.github.com>
2023-09-20 18:40:35 +0400

    work on button

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,6 +4,13 @@ import './Calculator.css';
 function Calculator() {
   const [display, setDisplay] = useState('0');
 
+  const buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+',
+  ];
+
   const handleButtonClick = (value) => {
    
   };
@@ -12,7 +19,15 @@ function Calculator() {
     <div className="calculator">
       <div className="display">{display}</div>
       <div className="buttons">
-       
+       {buttons.map((button) => (
+        <button 
+        key = {button}
+        className='button'
+        onClick={() => handleButtonClick(button)}
+        >
+            {button}
+        </button>
+       ))}
       </div>
     </div>
   );
